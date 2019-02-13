66bc21616428ba4aa977121937e9f69faf0eade7
Tom Bilbe <tbilbe@gmail.com>
2019-02-13 22:06:03 +0000

    completed num to reversed digits - but used toEqual?

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,5 +1,13 @@
 const numberToReversedDigits = (number) => {
+  const reversedNums = [];
+  const stringNumsAr = number.toString().split('');
 
+  for (let i = stringNumsAr.length - 1; i >= 0; i--) {
+
+    reversedNums.push(parseInt(stringNumsAr[i], 10));
+  }
+
+  return reversedNums;
 }
 
 module.exports = numberToReversedDigits;
