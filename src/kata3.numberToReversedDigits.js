5f0cd2d80587f1525faa5c99723ea5598cd02bab
Andrea Hurren <andrea@codesigned.co.uk>
2019-02-14 13:03:16 +0000

    numberToReverse test done and passed

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,5 +1,7 @@
 const numberToReversedDigits = (number) => {
-
-}
+    let str = String(number);
+    let arr = Array.from(str).reverse();
+    return arr.map( str => parseInt(str) );
+};
 
 module.exports = numberToReversedDigits;
