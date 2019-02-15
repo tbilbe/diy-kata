a2c0ecf8b40c0ae2f6fa6704453944d1b6390227
Andrea Hurren <andrea@codesigned.co.uk>
2019-02-15 12:35:17 +0000

    cat dog years test done and passed

--- __tests__/kata2.booleanToWord.test.js
+++ __tests__/kata2.booleanToWord.test.js
@@ -3,7 +3,6 @@ const { booleanToWord } = require('../src');
 describe('booleanToWord', () => {
   it('returns Yes when passed true', () => {
     expect(booleanToWord(true)).toBe('Yes');
-  
   });
 
   it('returns No when passed false', () => {
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,4 +1,27 @@
 const humanCatDogYears = (number) => {
+   // human age input
+   const humanYears = number;
+
+   // First and second year for cat and dog;
+   const catDogFirstSeconodYear = 15 + 9;;
+
+   // Cat years after first 2 years;
+   const catOtherYears = (humanYears - 2) * 4;
+
+   // Dog years after first 2 years;
+   const dogOtherYears = (humanYears - 2) * 5;
+
+   // Total dog years converted from humanYears input
+   let totalCatYears = catDogFirstSeconodYear + catOtherYears;
+
+   // Total dog years converted from humanYears input
+   let totalDogYrs = catDogFirstSeconodYear + dogOtherYears;
+
+   // Create the array of human, cat and dog years
+   let humanCatDogArr = [number,totalCatYears,totalDogYrs]
+
+   // output array above;
+   return humanCatDogArr;
 
 }
 
