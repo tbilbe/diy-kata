b3686ddbfeaf13b4d7259a9441d089ef6f6b8cb4
Tom Bilbe <tbilbe@gmail.com>
2019-02-15 13:33:05 +0000

    finished the humanCatDog section -> need to refactor

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
 const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears returns converted number in the array of human cat and dog years', () => {
+  it('function returns array -> numbers converted to the correct ages', () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+  });
+});
+
+describe('', () => {
+
+});
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,27 @@
 const humanCatDogYears = (number) => {
-
+  const array = []
+  const catAge = (number) => {
+    if (number > 2) {
+      return ((number - 2) * 4) + 24
+    } else if (number === 2) {
+      return 24
+    } else if (number < 2 && number > 0) {
+      return 15
+    }
+  }
+  const dogAge = (number) => {
+    if (number > 2) {
+      return ((number - 2) * 5) + 24
+    } else if (number === 2) {
+      return 24
+    } else if (number < 2 && number > 0) {
+      return 15
+    }
+  }
+  array.push(number)
+  array.push(catAge(number))
+  array.push(dogAge(number))
+  return array
 }
 
 module.exports = humanCatDogYears;
