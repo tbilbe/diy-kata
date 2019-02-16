07c5b679b2d6e9e11a8158ff8fcf0e28f8a8db31
Andrea Hurren <andrea@codesigned.co.uk>
2019-02-16 21:57:01 +0000

    finished joinNames and findEmployee TDD katas, all passed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,16 +1,19 @@
 const joinNames = (namesObj) => {
-
-    const resultsArray = namesObj.map((itemObj, i, arr) => {
-        if (arr.length - 1 === i) {
-            return '& ' + itemObj.name;
-        } else if (arr.length - 2 === i) {
-            return itemObj.name + ' ';
-        } else {
-            return itemObj.name + ', ';
+  // here I'm using the optional index and source array parameters
+  const resultsArr = namesObj.reduce((accName, currVal, idx, srcArr) => {
+    // if first item in array which starts with space:
+        if(accName === ''){
+          return accName + currVal.name;
+    // else if index is at the last item in array:
+        } else if (idx === srcArr.length - 1) {
+          return accName + ' & ' + currVal.name;
+        }else {
+          return accName + ', ' + currVal.name;
         }
-    });
+  },'');
 
-    return resultsArray.join('');
+  // return the result array as a string with String constructor
+  return String(resultsArr);
 }
 
 module.exports = joinNames;
--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,5 +1,7 @@
 const getEmployerRole = (employeeName, employees) => {
+    const findEmployee = employees.find(employee => employeeName === employee.name);
 
+    return findEmployee.role;
 }
 
 module.exports = getEmployerRole
