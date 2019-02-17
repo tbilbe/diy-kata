a972640e995daf3ba6fa043360703f103089b775
Geevan Patel <geevan.peter.patel@gmail.com>
2019-02-17 12:28:05 +0000

    Number to reversed digits Completed

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,6 +2,8 @@ const { numberToReversedDigits } = require('../src');
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
-    
+    expect(numberToReversedDigits(12345)).toEqual([5, 4, 3, 2, 1]);
+    expect(numberToReversedDigits(13579)).toEqual([9, 7, 5, 3, 1]);
+    expect(numberToReversedDigits(54321)).toEqual([1, 2, 3, 4, 5]);
   });
 });
--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,16 @@
 const booleanToWord = (boolean) => {
+    return (boolean === true ? 'Yes' : 'No');
+};    
+    
+    // or the long way...
+  //  if (boolean === true) {
+  //          return 'Yes'
+  //      } else {
+  //          return 'No'
+  //      }
+  //     };
 
-}
+       
 
 module.exports = booleanToWord;
+ 
