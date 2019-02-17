df2a6790974907467848254bffdc5709c105d354
Geevan Patel <geevan.peter.patel@gmail.com>
2019-02-17 21:05:28 +0000

    joinNames done

--- __tests__/kata6.joinNames.test.js
+++ __tests__/kata6.joinNames.test.js
@@ -2,6 +2,8 @@ const { joinNames } = require('../src');
 
 describe('joinNames', () => {
   it('returns string of names, seperated by commas and an ampersand', () => {
-    
+    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}])).toEqual('Bart, Lisa & Maggie');
+    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}])).toEqual('Bart & Lisa');
+    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}, {name: 'Marge'}, {name: 'Homer'}])).toEqual('Bart, Lisa, Maggie, Marge & Homer');
   });
 });
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,5 @@
 const joinNames = (namesObj) => {
-
-}
+  return namesObj.map(a => a.name).join(', ').replace(/, ([^,]*)$/, ' & $1');
+};
 
 module.exports = joinNames;
