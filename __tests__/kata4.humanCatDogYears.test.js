13943456718f7eff05443b481c31719fe3f4e8c9
Geevan Patel <geevan.peter.patel@gmail.com>
2019-02-17 13:38:46 +0000

    Human Dog Cat Years Done

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+describe('numberToReversedDigits', () => {
+it('Returns an array of human, cat and dog years', () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+    expect(humanCatDogYears(50)).toEqual([50, 216, 264])
+    });
+});
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,15 @@
 const humanCatDogYears = (number) => {
+let Human = number;
+let Cat = 24+((number-2)*4);
+let Dog = 24+((number-2)*5);
+return [Human, Cat, Dog];
+};
 
-}
+// formulas
+// 15+9+((number-2)*4) = cat
+// 24+((number-2)*4) = cat
 
+// change 4 to 5 for the Dog
+
+ 
 module.exports = humanCatDogYears;
